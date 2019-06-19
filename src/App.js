b90ac2ae9db619e41555c0468dce4c60fa028b14
Spencer Hurley <hurley.s@husky.neu.edu>
2019-06-19 16:22:30 -0400

    POST review to /api/test endpoint

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Button, InputGroup, FormControl, Col, Container, Form } from 'react-bootstrap';
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.database = this.database.bind(this);
+    this.changeReview = this.changeReview.bind(this);
+    this.state = {
+      movie_id: '',
+      reviewer_id: '',
+      rating: '',
+      date_posted: '',
+      description: ''
+    }
   }
 
   database() {
-    fetch("http://127.0.0.1:5000/api/get_chars")
-    .then(res => res.json());
+    fetch('http://127.0.0.1:5000/api/test', {
+      method: 'post',
+        headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)});
+    //fetch("http://127.0.0.1:5000/api/get_chars")
+    //.then(res => res.json());
+    console.log(this.state)
+  }
+
+  changeReview(fieldname, newvalue) {
+    this.state[fieldname] = newvalue;
   }
 
   render() {
@@ -19,27 +39,73 @@ export default class App extends React.Component {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <form>
-          <label>
-            Name:
-            <input type="text" name="name" />
-          </label>
-        </form>
-        <button onClick={this.database}>
-          Hit DB 
-        </button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3> New Review </h3>
+        <hr/>
+        <Container>
+          <Col md={{ span: 6, offset: 3 }}>
+          <Form>
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+                <InputGroup.Text>Movie ID</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                onChange={(e) => this.changeReview("movie_id", e.target.value)}
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+                <InputGroup.Text>Reviewer ID</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                onChange={(e) => this.changeReview("reviewer_id", e.target.value)}
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+                <InputGroup.Text>Rating     </InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                onChange={(e) => this.changeReview("rating", e.target.value)}
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+                <InputGroup.Text>Date</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                onChange={(e) => this.changeReview("date_posted", e.target.value)}
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+                <InputGroup.Text>Review Body</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                onChange={(e) => this.changeReview("description", e.target.value)}
+                as="textarea"
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+          </Form>
+        </Col>
+        <Button onClick={this.database} type="submit">Create Review</Button>
+        </Container>
       </header>
+      <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossOrigin="anonymous"
+      />
     </div>
     );
   }
