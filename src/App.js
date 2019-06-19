25051690bb48825926055b1d28a34c05918aad7e
Spencer Hurley <hurley.s@husky.neu.edu>
2019-06-19 12:39:30 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.database = this.database.bind(this);
+  }
+
+  database() {
+    fetch("http://127.0.0.1:5000/api/get_chars")
+    .then(res => res.json());
+  }
+
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <form>
+          <label>
+            Name:
+            <input type="text" name="name" />
+          </label>
+        </form>
+        <button onClick={this.database}>
+          Hit DB 
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +41,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    );
+  }
 }
 
-export default App;
